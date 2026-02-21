de0e7d0cc564c85009d39c1d5b1c69f018652130
Kabileshwarankabil <kabileshwaran1896@gmail.com>
2026-02-21 17:32:47 +0530

    Added Services module

--- src/com/citybookshop/service/BookService.java
+++ src/com/citybookshop/service/BookService.java
@@ -1,5 +1,93 @@
 package com.citybookshop.service;
 
+import com.citybookshop.model.Book;
+import java.util.ArrayList;
+import java.util.List;
 public class BookService {
+    private static final String FILE_PATH="src/data/books.txt";
+    private FileHandler fileHandler;
+    public BookService(){
+        this.fileHandler=new FileHandler();
+    }
+    public List<Book> getAllBooks(){
+        List<Book> books= new ArrayList<>();
+        List<String> lines=fileHandler.readline(FILE_PATH);
 
+        for(String line:lines){
+            Book book = lineToBook(line);
+            if(book!=null){
+                books.add(book);
+            }
+        }
+        return books;
+    }
+    public boolean addBook(String title,String author,double price,int quantity,String categoryName){
+        if(bookExists(title)){
+            return false;
+        }
+        String newId=generateId();
+        String newLine=newId+"|"+title+"|"+author+"|"+price+"|"+quantity+"|"+categoryName;
+        fileHandler.appendLine(FILE_PATH,newLine);
+        return true;
+    }
+
+    private String generateId() {
+        List<String> lines=fileHandler.readline(FILE_PATH);
+        int count=lines.size()+1;
+        return String.format("BK%03d",count);
+    }
+
+    private boolean bookExists(String title) {
+        for(Book book :getAllBooks()){
+            if(book.getTitle().equalsIgnoreCase(title)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Book lineToBook(String line) {
+        try{
+            String[] parts=line.split("\\|");
+            if(parts.length==6){
+                String bookId=parts[0];
+                String title=parts[1];
+                String author=parts[2];
+                double price= Double.parseDouble(parts[3]);
+                int quantity=Integer.parseInt(parts[4]);
+                String categoryName=parts[5];
+                return new Book(bookId,title,author,price,quantity,categoryName);
+            }
+        }catch(Exception e){
+            System.out.println("Error reading file: "+FILE_PATH);
+        }
+        return null;
+    }
+    public List<Book> searchByTitle(String title){
+        List<Book> results=new ArrayList<>();
+        for(Book book:getAllBooks()){
+            if(book.getTitle().equalsIgnoreCase(title)){
+                results.add(book);
+            }
+        }
+        return results;
+    }
+    public List<Book> searchByCategory(String categoryName){
+        List<Book> results=new ArrayList<>();
+        for(Book book:getAllBooks()){
+            if(book.getCategoryName().equalsIgnoreCase(categoryName)){
+                results.add(book);
+            }
+        }
+        return results;
+    }
+    public List<Book> getLowStock(){
+        List<Book> results=new ArrayList<>();
+        for(Book book: getAllBooks()){
+            if(book.getQuantity()<=5){
+                results.add(book);
+            }
+        }
+        return results;
+    }
 }
--- src/com/citybookshop/service/CategoryService.java
+++ src/com/citybookshop/service/CategoryService.java
@@ -1,5 +1,76 @@
 package com.citybookshop.service;
-
+import com.citybookshop.model.Category;
+import java.util.List;
+import java.util.ArrayList;
 public class CategoryService {
+    private static final String FILE_PATH="src/data/categories";
+    private FileHandler fileHandler;
+    public CategoryService(){
+        fileHandler=new FileHandler();
+    }
+    public List<Category> getAllCategory(){
+        List<Category> categories=new ArrayList<>();
+        List<String> lines=fileHandler.readline(FILE_PATH);
+        for(String line:lines){
+            Category category=lineToCategory(line);
+            if(category!=null){
+                categories.add(category);
+            }
+        }
+        return categories;
+    }
 
+    private Category lineToCategory(String line) {
+        try{
+            String[] parts=line.split("\\|");
+            if(parts.length==3){
+                String categoryId=parts[0];
+                String categoryName=parts[1];
+                String description=parts[2];
+                return new Category(categoryId,categoryName,description);
+            }
+        }catch(Exception e){
+            System.out.println("Error Reading File: "+FILE_PATH);
+        }
+        return null;
+    }
+    public List<String> getCategoryName(){
+        List<String> names=new ArrayList<>();
+        for(Category category:getAllCategory()){
+            names.add(category.getCategoryName());
+        }
+        return names;
+    }
+    public boolean addCategory(String name,String description){
+        if(categoryExists(name)){
+            return false;
+        }
+        String newId=generateId();
+        String newLine=newId+"|"+name+"|"+description;
+        fileHandler.appendLine(FILE_PATH,newLine);
+        return true;
+    }
+
+    public Category searchByName(String name){
+        for(Category category:getAllCategory()){
+            if(category.getCategoryName().equalsIgnoreCase(name)){
+                return category;
+            }
+        }
+        return null;
+    }
+    private String generateId() {
+        List<String> lines=fileHandler.readline(FILE_PATH);
+        int count=lines.size()+1;
+        return String.format("CA%03d",count);
+    }
+
+    private boolean categoryExists(String name) {
+        for(Category category:getAllCategory()){
+            if(category.getCategoryName().equalsIgnoreCase(name)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
