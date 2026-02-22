b38efcc72e37342f115b5063258e434a6861f77d
Kabileshwarankabil <kabileshwaran1896@gmail.com>
2026-02-22 10:20:40 +0530

    Added Dashboard.fxml

--- src/com/citybookshop/Main.java
+++ src/com/citybookshop/Main.java
@@ -1,7 +1,28 @@
 package com.citybookshop;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
 
+        // Step 1 — Load the Login screen
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("view/Login.fxml")
+        );
+
+        // Step 2 — Create the scene with size
+        Scene scene = new Scene(loader.load(), 700, 560);
+
+        // Step 3 — Set up the window
+        stage.setTitle("City Bookshop");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
     }
 }
